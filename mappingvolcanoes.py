c6ddf7535f0de9f3e01ce1b0b8959f1a7d1a908c
kat007 <katherine.gu@gmail.com>
2018-11-21 14:35:57 +1300

    Add files via upload

--- mappingvolcanoes.py
+++ mappingvolcanoes.py
@@ -1,7 +1,7 @@
 import pandas
 import folium
 
-print(dir(folium))
+#print(dir(folium))
 
 
 data = pandas.read_csv("WorldVolcanoes.txt")
@@ -21,8 +21,8 @@ map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
 fgv = folium.FeatureGroup(name="Volcanoes")
 for lt, ln, el in zip(lat, lon, elev):
-    #fgv.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color=colour_producer(el))))
-    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el) + " m", fill_color=colour_producer(el), color='grey', fill_opacity=0.7))
+    fgv.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color=colour_producer(el))))
+    #fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el) + " m", fill_color=colour_producer(el), color='grey', fill_opacity=0.7))
 
 #adding a geojson polygon layer: choropleth with colour
 fgp = folium.FeatureGroup(name="Population")
@@ -37,19 +37,4 @@ map.add_child(fgp)
 
 map.add_child(folium.LayerControl())
 map.save("Map1.html")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
